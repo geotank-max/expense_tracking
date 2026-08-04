20a39ff59ad3e3d9cae1e8fa61ca94c566856f41
Eng Tangk <geotangk@gmail.com>
2026-08-04 12:16:29 +0700

    define relationship in model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['user_id', 'category_id', 'type', 'amount', 'transaction_date', 'description'];
+
+    protected $casts = [
+        'transaction_date' => 'date',
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
